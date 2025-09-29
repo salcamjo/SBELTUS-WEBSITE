7b1e850927d794052a5901946350b4e53598ec11
José Fernando Salinas <fernandosalinas@hotmail.com>
2025-09-29 14:16:41 -0500

    Ajuste tamaño foto en sección About

--- scripts.js
+++ scripts.js
@@ -18,3 +18,15 @@ function setLanguage(lang) {
 window.onload = () => {
   loadLanguage(currentLang);
 };
+// Esperar a que el DOM esté listo
+document.addEventListener("DOMContentLoaded", () => {
+  const menuToggle = document.querySelector(".menu-toggle"); // botón ☰
+  const nav = document.querySelector(".nav"); // menú de navegación
+
+  if (menuToggle && nav) {
+    menuToggle.addEventListener("click", () => {
+      nav.classList.toggle("active"); // abre/cierra el menú
+    });
+  }
+});
+
